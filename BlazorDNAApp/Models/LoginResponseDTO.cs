cec94a5ed8e763ffd0fabe9dcf3501ae97a75b5b
Natália Bruno Rabelo <nataliabruno@id.uff.br>
2023-01-27 21:58:54 -0300

    Login pronto

---
+++ BlazorDNAApp/Models/LoginResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorDNAApp.Models
+{
+    public class LoginResponseDTO
+    {
+        public string Token { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
